75a337f903dc73b032af12932c5a46f2411fe041
Mary Kate Skitka <mkskitka@gmail.com>
2022-03-25 11:00:39 -0400

    adding template component

---
+++ src/Components/ComponentTemplate/ComponentTemplate.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./ComponentTemplate.css"
+/*    other imports Here     */
+
+function ProjectSummary(props) {
+
+    const {} = props;
+    // State
+    //const active_project_id = useSelector(state => state.active_project);
+
+
+
+    useEffect(() => {
+    }, [])
+
+
+    return (
+        <div>
+            Testing
+        </div>
+    );
+
+}
+
+export default ProjectSummary;
